e3d9075b46c6e8e1a1410e563536605de4961e88
candide guevara <candide-guevara@users.noreply.github.com>
2023-08-27 12:53:49 +0200

    update aoe2 patch scripts for proton8

--- configuration/steam/common.sh
+++ configuration/steam/common.sh
@@ -2,7 +2,7 @@
 STEAM_LIB_ROOT='/media/llewelyn_data_b/SteamLibrary'
 VS_RUNTIME_DOWN_SRV="aka.ms"
 #PROTON_ROOT="$STEAM_LIB_ROOT/steamapps/common/Proton 5.13"
-PROTON_ROOT="$STEAM_LIB_ROOT/steamapps/common/Proton 6.3"
+PROTON_ROOT="$STEAM_LIB_ROOT/steamapps/common/Proton 8.0"
 # Works for both 5 and 6 version of runtime
 CURRENT_PROTON_SETTINGS="user_settings_5_13.py"
 
@@ -39,6 +39,14 @@ verify_cert() {
   fi
 }
 verify_cert "$VS_RUNTIME_DOWN_SRV"
+verify_cert "download.microsoft.com"
+
+patch_visual_studio_runtime_2015() {
+  local copy_filter_rx="${1:-'.*'}"
+  patch_visual_studio_runtime_helper \
+    "https://download.microsoft.com/download/9/3/F/93FCF1E7-E6A4-478B-96E7-D4B285925B00/vc_redist.x64.exe" \
+    "$copy_filter_rx"
+}
 
 patch_visual_studio_runtime_2017() {
   patch_visual_studio_runtime_helper "https://$VS_RUNTIME_DOWN_SRV/vs/15/release/vc_redist.x64.exe"
@@ -48,10 +56,15 @@ patch_visual_studio_runtime_2019() {
   patch_visual_studio_runtime_helper "https://$VS_RUNTIME_DOWN_SRV/vs/16/release/vc_redist.x64.exe"
 }
 
+patch_visual_studio_runtime_2022() {
+  patch_visual_studio_runtime_helper "https://$VS_RUNTIME_DOWN_SRV/vs/17/release/vc_redist.x64.exe"
+}
+
 #cf https://steamcommunity.com/app/813780/discussions/3/3938911974192987545/
 #cf https://github.com/Winetricks/winetricks/blob/master/src/winetricks
 patch_visual_studio_runtime_helper() {
   local download_url="$1"
+  local copy_filter_rx="${2:-'.*'}"
   local tmp_cabextracted="`mktemp -d`"
   local system32_dir="$EMU_CDRIVE_ROOT/windows/system32"
   local backup_dir="$system32_dir/backup_dll"
@@ -64,17 +77,17 @@ patch_visual_studio_runtime_helper() {
     cabextract vc_redist.x64.exe || exit 1
     cabextract a10 || exit 1
     #cabextract a11 || exit 1
-    local -a libs=( `find -type f -iname '*.dll' | sed -r 's/^\.\///' | sort -u` )
+    local -a libs=( `find -type f -iname '*.dll' | sed -r 's/^\.\///' | sort -u | grep -E "$copy_filter_rx"` )
 
     for library in "${libs[@]}"; do
-      local alt_name="${library//_/-}"
-      if [[ "$alt_name" != "$library" ]]; then
-        ln -s -T "$library" "$alt_name"
-        mv --no-clobber "$system32_dir/$alt_name" "$backup_dir" 2> /dev/null
-        cp -f "$alt_name" "$system32_dir"
-      fi
+      chmod ugo+x "$library"
       mv --no-clobber "$system32_dir/$library" "$backup_dir" 2> /dev/null
       cp -f "$library" "$system32_dir"
+
+      local alt_name="${library//_/-}"
+      if [[ "$alt_name" != "$library" ]]; then
+        ln -s -T "$system32_dir/$library" "$system32_dir/$alt_name"
+      fi
     done
   popd
 }
--- configuration/steam/patch_aoe2_de.sh
+++ configuration/steam/patch_aoe2_de.sh
@@ -1,11 +1,35 @@
 ## Attempts to fix age of empires 2 multiplayer.
 source common.sh || exit 2
 
+# see https://github.com/ValveSoftware/Proton/issues/3189#issuecomment-1610974028
+tweak_encryption_validation_lib() {
+  pushd /tmp
+  git clone git://source.winehq.org/git/wine.git
+  cd wine
+  ./configure CFLAGS="-march=native -O3 -pipe -fstack-protector-strong" --enable-win64
+  cd dlls/wintrust
+  sed -ri 's/(if \(IsEqualGUID\(ActionID, &published_software\)\))/if(1) { err = 0; } else \1/' wintrust_main.c
+  make
+  popd
+  pushd "$PROTON_ROOT"
+  mv dist/lib64/wine/x86_64-windows/wintrust.dll dist/lib64/wine/x86_64-windows/broken_wintrust.broken
+  cp -f /tmp/wine/dlls/wintrust/x86_64-windows/wintrust.dll dist/lib64/wine/x86_64-windows/wintrust.dll
+  popd
+}
+
+# see https://forums.ageofempires.com/t/skipping-intro-video-of-aoe2-hd-and-de-upon-launch-for-steam/81525
+set_skip_intro_in_launch_opts() {
+  echo "[ERROR] set SKIPINTRO cannot be done manually :/"
+}
+
 set_environment_vars "813780" AoE2DE
 
-patch_visual_studio_runtime_2019
+patch_visual_studio_runtime_2015 ucrtbase
 # https://www.reddit.com/r/aoe2/comments/dwuplr/how_to_run_age_of_empires_2_definitive_edition_on/
-#rename_game_data_dir_matching movies 
-copy_user_settings
+# latest guide: https://aoe2.arkanosis.net/linux/#aoe2_de_online_multiplayer
+# apparently this must be run after the game is launched once?
+rename_game_data_dir_matching movies 
+tweak_encryption_validation_lib
+#copy_user_settings
 echo "ALL DONE"
 
